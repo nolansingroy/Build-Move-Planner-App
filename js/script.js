820114c4f4a64ae9d5798c921afba1f5e1048794
duke <nolansingroy>
2018-01-15 20:09:33 -0600

    Wiki Media Api sucessfully added Wikipedia api using jsonp

--- js/script.js
+++ js/script.js
@@ -41,7 +41,25 @@ function loadData() {
    }).error(function(e){
      $nytHeaderElem.text('New York Times Article could not be displayed')
    });
+   var wikiUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + cityStr + '&format=json&callback=wikiCallback';
 
+
+   $.ajax({
+       url:wikiUrl,
+       dataType :"jsonp",
+       success : function(response){
+           var articleList = response[1];
+
+           for  (var i =0 ; i < articleList.length; i++) {
+               articleStr = articleList[i];
+               var url = "http://en.wikipedia.org/wiki/" + articleStr;
+               $wikiElem.append('<li> <a href = "'+ url + ' ">' + articleStr + '</a></li>');
+           };
+       }
+       //error handling
+   }).error(function(e){
+       $wikiElem.text(' failed to get wikipedia resources');
+   } );
     // YOUR CODE GOES HERE!
 
     return false;
